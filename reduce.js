824669a8e8e68b0ce1e5fbb01b471d6c539703e4
llewelyn-williams <llewelyn.williams@googlemail.com>
2021-07-20 19:40:21 +0000

    chore: Add comments to improve my understanding
    Video Part 1.
    In order to consolidate my undestanding of how the .reduce() method
    works.

--- reduce.js
+++ reduce.js
@@ -5,7 +5,41 @@
  */
 
 
+// Reduce reduces vales in an array to a single value.
+// acc stands for "accumulator"
+// curr staand for "currentValue"
+
+// .reduce requires those above two as paramaters in 
+// its callback function.
+
+// By default the acc will initialise with the first element
+// of the Array, e.g. nums[0]
+// This in turn means that the accumilation process (and 
+// therefore the curr) starts with the second elment in the
+// array e.g nums[1]
+
+// Therefore the first pass produces this result in the example
+// without specifying and initialValue
+// Accumulator:  0 Current value:  1 Total:  1
+
+// However you can specify an inital value for the acc
+// as a parameter after the callback function.
+
+// Specifying stops acc using the first value of the array
+// and forces it to use the specified value instead, leaving
+// the first value free to be used by curr.
+
+// In the below example spcifying an initialValue of 10
+// results in an output of 20 (after five passes)
+
 // Summing an array of numbers:
+
+// const nums = [0, 1, 2, 3, 4];
+// let sum = nums.reduce((acc, curr) => acc + curr, 0);
+// console.log(sum);
+
+// Tidy version commented out above.
+
 const nums = [0, 1, 2, 3, 4];
 let sum = nums.reduce((acc, curr) => {
   console.log(
@@ -14,8 +48,10 @@ let sum = nums.reduce((acc, curr) => {
     "Total: ", acc + curr
   );
   return acc + curr;
-});
+}, 10);
 console.log(sum);
+
+
 
 const teamMembers = [
   {
